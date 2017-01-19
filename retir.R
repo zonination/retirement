54b6434cf07dfc531a33dd285f90b424a25de006
Zoni Nation <zonination@gmail.com>
2017-01-19 11:33:41 -0500

    Add files via upload

--- retir.R
+++ retir.R
@@ -1,11 +1,11 @@
+# Assumptions:
+real.r = 5.0  # Real return from portfolio, in stocks, bonds, etc. as percent.
+swr    = 4.0  # "Safe Withdrawal Rate" from portfolio, as percent.
+
 # Load libraries, load assumptions
 library(ggplot2)
 library(scales)
 library(RColorBrewer)
-
-# Assumptions:
-real.r = 5.0  # Real return from portfolio, in stocks, bonds, etc. in percent.
-swr    = 4.0  # "Safe Withdrawal Rate" from portfolio
 
 #######################
 # PART 1: Calculation #
@@ -43,17 +43,17 @@ df$spend <- df$l.income/(1-df$save.r)
 # Plot the final results, as Income Multiplier
 ggplot(df,aes(save.r,years))+
   # Using logscale since it's easier to visualize on this heatmap.
-  geom_raster(aes(fill=log(l.income)/log(2)), interpolate=T, alpha=.8)+
+  geom_raster(aes(fill=log(l.income)/log(2)), interpolate=T, alpha=.7)+
   geom_contour(aes(z=log(l.income)/log(2)),color="black",size=.5, binwidth=1)+
-  scale_fill_gradientn("Living Income\nAfter Retirement",
+  scale_fill_gradientn("Desired Income\nAfter Retirement",
                        colors = brewer.pal(9,"PiYG"),
                        limits=c(-4,4),
-                       breaks=c(-4:4),
+                       breaks=c(-4:4),.
                        labels=c(".06 x income",".13 x income",".25 x income",".5 x income","1 x income","2 x income","4 x income","8 x income", "16 x income"),
                        oob=scales::squish)+
   scale_x_continuous(breaks=seq(0,1,.1), labels=scales::percent, limits=c(0,1))+
   scale_y_continuous(breaks=seq(0,60,10), limits=c(-1,61))+
-  labs(title="Retirement, by Income",
+  labs(title="Retirement, by Desired Income",
        subtitle="Assuming 5% portfolio earnings, 4% safe withdrawal rate",
        x="Savings rate, as % of income",
        y="Number of years saved",
@@ -64,15 +64,18 @@ ggplot(df,aes(save.r,years))+
   annotate("text", x=.5, y=26, label="1 x income", size=3, angle=-20)+
   annotate("text", x=.5, y=17, label=".5 x income", size=3, angle=-15)+
   annotate("text", x=.5, y=10, label=".25 x income", size=3, angle=-10)+
-  theme_bw()
+  theme_bw()+
+  theme(plot.caption=element_text(size=7, vjust=0))+
+  theme(legend.text =element_text(size=8))+
+  theme(legend.title=element_text(size=10))
 ggsave("income.png",height=6,width=10,dpi=100,type="cairo-png")
 
 # Plot the final results, as Spend Multiplier
 ggplot(df,aes(save.r,years))+
   # Using logscale since it's easier to visualize on this heatmap.
-  geom_raster(aes(fill=log(spend)/log(2)), interpolate=T, alpha=.8)+
+  geom_raster(aes(fill=log(spend)/log(2)), interpolate=T, alpha=.7)+
   geom_contour(aes(z=log(spend)/log(2)),color="black",size=.5, binwidth=1)+
-  scale_fill_gradientn("Living Income\nAfter Retirement",
+  scale_fill_gradientn("Desired Budget\nAfter Retirement",
                        colors = brewer.pal(9,"PiYG"),
                        limits=c(-4,4),
                        breaks=c(-4:4),
@@ -80,7 +83,7 @@ ggplot(df,aes(save.r,years))+
                        oob=scales::squish)+
   scale_x_continuous(breaks=seq(0,1,.1), labels=scales::percent, limits=c(0,1))+
   scale_y_continuous(breaks=seq(0,60,10), limits=c(-1,61))+
-  labs(title="Retirement, by Budget",
+  labs(title="Retirement, by Desired Budget",
        subtitle="Assuming 5% portfolio earnings, 4% safe withdrawal rate",
        x="Savings rate, as % of income",
        y="Number of years saved",
@@ -91,5 +94,8 @@ ggplot(df,aes(save.r,years))+
   annotate("text", x=.5, y=17, label="1 x budget", size=3, angle=-25)+
   annotate("text", x=.5, y=10, label=".5 x budget", size=3, angle=-20)+
   annotate("text", x=.5, y=5.75, label=".25 x budget", size=3, angle=-12.5)+
-  theme_bw()
-ggsave("spend.png",height=6,width=10,dpi=100,type="cairo-png")
+  theme_bw()+
+  theme(plot.caption=element_text(size=7, vjust=0))+
+  theme(legend.text =element_text(size=8))+
+  theme(legend.title=element_text(size=10))
+ggsave("budget.png",height=6,width=10,dpi=100,type="cairo-png")
